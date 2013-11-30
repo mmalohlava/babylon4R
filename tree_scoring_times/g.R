fc4beb0b520d72908ecff0b346aae312acb7a787
mmalohlava <michal.malohlava@gmail.com>
2013-11-29 19:41:33 -0800

    Minor updatte

--- tree_scoring_times/g.R
+++ tree_scoring_times/g.R
@@ -1,16 +1,18 @@
 #!/usr/bin/Rscript
 
 library(ggplot2)
-r50t_5d  <- read.table("GBM_covtype_50trees_5depth.csv", header=T, sep=",")
-r50t_10d <- read.table("GBM_covtype_50trees_10depth.csv", header=T, sep=",")
-r100t_5d <- read.table("GBM_covtype_100trees_5depth.csv", header=T, sep=",")
-r100t_10d <- read.table("GBM_covtype_100trees_10depth.csv", header=T, sep=",")
+r50t_5d  <- read.table("GBM_50t_5d.csv", header=T, sep=",")
+r50t_10d <- read.table("GBM_50t_10d.csv", header=T, sep=",")
+r100t_5d <- read.table("GBM_100t_5d.csv", header=T, sep=",")
+r100t_10d <- read.table("GBM_100t_10d.csv", header=T, sep=",")
+r1000t_5d <- read.table("GBM_1000t_5d.csv", header=T, sep=",")
 
 fgdata <- data.frame(iter=r50t_5d$iter,
                     r50t_5d=r50t_5d$time_per_row_tree,
                     r50t_10d=r50t_10d$time_per_row_tree, 
                     r100t_5d=r100t_5d$time_per_row_tree,
-                    r100t_10d=r100t_10d$time_per_row_tree)
+                    r100t_10d=r100t_10d$time_per_row_tree,
+                    r1000t_5d=r1000t_5d$time_per_row_tree)
 summary(fgdata)
 
 mkdf  <- function(dataset) {
@@ -28,24 +30,24 @@ plot_g <- function(df, color, min, max, warmup,title) {
   xlab("iterations") + ylab("scoring time per tree and row (nanosecond)") + ggtitle(title)
 }
 
+mktitle <- function(trees, depth, features) {
+  return( sprintf("GBM: %d trees, %d depth, %d features\nOne iteration: scoring 193672 rows",trees, depth, features) )
+}
+
 ds <- mkdf(r50t_5d)
-plot_g (ds, "red", 0, 1500, 1000, "GBM: 50 trees, 5 depth\nIteration: scoring on 100 rows") 
+plot_g (ds, "red", 200, 400, 50, mktitle(50,5,7)) #"GBM: 50 trees, 5 depth, 7 features\nIteration: scoring on 193672 rows") 
 
 ds <- mkdf(r100t_5d)
-plot_g (ds, "red", 0, 1500, 1000, "GBM: 100 trees, 5 depth\nIteration: scoring on 100 rows") 
+plot_g (ds, "red", 200, 400, 50, mktitle(100,5,7)) # "GBM: 100 trees, 5 depth, 7 features\nIteration: scoring on 193672 rows") 
+
+ds <- mkdf(r1000t_5d)
+plot_g (ds, "red", 1000, 1400, 50, mktitle(1000,5,7)) #"GBM: 1000 trees, 5 depth, 7 features \nIteration: scoring on 193672 rows") 
 
 ds <- mkdf(r50t_10d)
-plot_g (ds, "red", 1000, 3500, 1000, "GBM: 50 trees, 10 depth\nIteration: scoring on 100 rows") 
+plot_g (ds, "red", 1500, 2500, 50, mktitle(50,10,7)) #"GBM: 50 trees, 10 depth, 7 features\nIteration: scoring on 193672 rows") 
 
 ds <- mkdf(r100t_10d)
-plot_g (ds, "red", 1000, 3500, 1000, "GBM: 100 trees, 10 depth\nIteration: scoring on 100 rows") 
-
-
- #geom_point(aes(y=r50t_10d),colour="blue", size=1, shape=21, fill="red") + 
-  #geom_point(aes(y=r100t_5d),colour="green", size=1, shape=21, fill="red") +
-  #geom_point(aes(y=r100t_10d),colour="yellow", size=1, shape=21, fill="red") + 
-#ggplot(data=r50t_5d, aes(x=iter, y=time_per_row_tree)) + geom_boxplot(outlier.shape=NA) + scale_y_continuous(limits = quantile(r50t_5d$time_per_row_tree, c(0.1, 0.9)))
-
+plot_g (ds, "red", 1500, 2500, 50, mktitle(100,10,7)) #"GBM: 100 trees, 10 depth\nIteration: scoring on 193672 rows") 
 
 #summary(r50t_5d$time_per_row_inter_tree)
 #ggplot(data=r50t_5d, aes(x=iter, y=time_per_row_inter_tree)) + geom_point(colour="red", size=1, shape=21, fill="red") + xlab("iterations") + ylab("scoring time per internal tree and row (nanosecond)")
